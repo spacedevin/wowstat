bafb934fa45b3e69111b9df8fcc431abcf5b575d
Devin Smith <github@arzynik.com>
2016-06-25 12:26:36 -0700

    stuff

--- app.js
+++ app.js
@@ -29,41 +29,42 @@ angular
 		});
 	})
 */
-	.run(($rootScope, $http) => {
-		$rootScope.regions = [
+	.run(() => {
+
+	})
+	.controller('main', ($scope, $http) => {
+		$scope.regions = [
 			{name: 'US, Oceania, Latin America & Brazil', value: 'us'},
 			{name: 'Europe & Russia', value: 'eu'},
 			{name: 'China', value: 'cn'},
 			{name: 'Taiwan', value: 'tw'},
 			{name: 'Korea', value: 'kr'}
 		];
-		$rootScope.realms = [];
-		$rootScope.selectPath = () => {
+		$scope.realms = [];
+		$scope.selectPath = () => {
 			mainProcess.selectDirectory((res) => {
 				if (!res) {
 					return;
 				}
-				$rootScope.$apply(($scope) => {
+				$scope.$apply(($scope) => {
 					$scope.options.path = res;
 				});
 			});
 		};
-/*
-		storage.clear((error) => {
-			if (error) throw error;
-		});
-		return;
-		*/
 
-		$rootScope.options = {
-			region: $rootScope.regions[0].value,
+		$scope.options = {
+			region: $scope.regions[0].value,
 			up: 5,
 			down: 1,
 			path: process.platform == 'darwin' ? '/Applications/World of Warcraft/World of Warcraft.app': 'C:\\Program Files\\World of Warcraft\\WoW.exe',
 			actionUp: 'notify',
 			actionDown: 'notify'
 		};
-		$rootScope.realms = [];
+			$scope.$watch('options', () => {
+				console.debug('options YAY');
+			}, true);
+
+		$scope.realms = [];
 
 		storage.get('options', (error, options) => {
 			if (error) throw error;
@@ -71,30 +72,31 @@ angular
 
 			if (!Object.keys(options).length) {
 				console.debug('no options');
-				storage.set('options', $rootScope.options, (error) => {
+				storage.set('options', $scope.options, (error) => {
 					if (error) throw error;
 					console.log(options);
 				});
 			} else {
 				console.debug('options exist');
-				$rootScope.$apply(($scope) => {
+				$scope.$apply(($scope) => {
 					$scope.options = options;
 				});
 			}
 
 
 		});
-			$rootScope.$watch('options', (oldval, newval) => {
-				console.debug('options changed', $rootScope.options);
-				storage.set('options', $rootScope.options, (error) => {
-					if (error) throw error;
-				});
+
+		$scope.$watch('options', (oldval, newval) => {
+			console.debug('options changed', $scope.options);
+			storage.set('options', $scope.options, (error) => {
+				if (error) throw error;
 			});
+		}, true);
 		var loadRealms = () => {
-			$http.get('https://' + $rootScope.options.region + '.api.battle.net/wow/realm/status?locale=en_US&apikey=hw9djbbcu2cjacq36swsdkmq7y6cfnnt').then((res) => {
-				$rootScope.realms = res.data.realms;
-				if (!$rootScope.options.realm) {
-					$rootScope.options.realm = $rootScope.realms[0].value;
+			$http.get('https://' + $scope.options.region + '.api.battle.net/wow/realm/status?locale=en_US&apikey=hw9djbbcu2cjacq36swsdkmq7y6cfnnt').then((res) => {
+				$scope.realms = res.data.realms;
+				if (!$scope.options.realm) {
+					$scope.options.realm = $scope.realms[0].value;
 				}
 			});
 		};
@@ -103,16 +105,16 @@ angular
 			if (error) throw error;
 
 			if (!Object.keys(realms).length) {
-				$rootScope.$apply(($scope) => {
+				$scope.$apply(($scope) => {
 					$scope.realms = realms;
 					if ($scope.options.realm) {
 					}
 				});
 			}
 
-			$rootScope.$watch('realms', (oldval, newval) => {
-				console.debug('realms changed', $rootScope.realms);
-				storage.set('realms', $rootScope.realms, (error) => {
+			$scope.$watch('realms', (oldval, newval) => {
+				console.debug('realms changed', $scope.realms);
+				storage.set('realms', $scope.realms, (error) => {
 					if (error) throw error;
 				});
 			});
