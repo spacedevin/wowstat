d2257e1ef344208fbb1b476588b54fd9b4604e95
Devin Smith <github@arzynik.com>
2016-07-20 08:15:19 -0700

    windows window size

--- app/main.js
+++ app/main.js
@@ -27,8 +27,8 @@ var launcher = new AutoLaunch({
 
 var createWindow = () => {
 	win = new BrowserWindow({
-		width: 398,
-		height: process.platform === 'darwin' ? 285 : 300,
+		width: process.platform === 'darwin' ? 398 : 410,
+		height: process.platform === 'darwin' ? 285 : 320,
 		titleBarStyle: 'hidden',
 		resizable: false,
 		title: 'WoW Stat'
